d7ad22f29dda90a679931a2dd0a16dc60b32fd71
Estudiante USC <estudiante@GTACAD.USC>
2022-09-19 18:54:22 -0500

    nando
    docker_1_0

---
+++ conecar_postgres/src/main/java/com/conexion/conecar_postgres/ConecarPostgresApplication.java
@@ -0,0 +1,13 @@
+package com.conexion.conecar_postgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConecarPostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConecarPostgresApplication.class, args);
+	}
+
+}
---
+++ conecar_postgres/src/test/java/com/conexion/conecar_postgres/ConecarPostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conexion.conecar_postgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConecarPostgresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
